f76927a74a5c4cc641622d4f591560c8fe668c3e
SeriousDim <dlykov2001@yandex.ru>
2023-04-22 21:52:03 +0500

    feat(frontend): sign up page WIP, some index.ts files added

--- frontend/mobile/src/2_processes/stacks/auth-stack/config/config.ts
+++
@@ -1,8 +0,0 @@
-import {LoginPage} from "../../../../3_pages/login-page";
-import {ScreenConfig} from "../../../../7_shared/lib/stack-creator";
-
-export const SCREENS: ScreenConfig = {
-  Login: {
-    component: LoginPage
-  }
-}
---
+++ frontend/mobile/src/6_entities/user/ui/sign-up-form/input-data.ts
@@ -0,0 +1,59 @@
+import {InputData} from '../../../../7_shared/ui/fields/input-field';
+import {SignUpFormData} from './types';
+
+export const INPUT_DATA: InputData<SignUpFormData>[] = [
+  {
+    name: 'name',
+    label: 'Имя',
+    type: '',
+    options: {
+      required: 'Вы не заполнили это поле'
+    },
+    placeholder: 'Имя'
+  },
+  {
+    name: 'surname',
+    label: 'Фамилия',
+    type: '',
+    options: {
+      required: 'Вы не заполнили это поле'
+    },
+    placeholder: 'Фамилия'
+  },
+  {
+    name: 'phone',
+    label: 'Телефон',
+    type: '',
+    options: {
+      required: 'Вы не заполнили это поле'
+    },
+    placeholder: '+7 (000) 000-00-00'
+  },
+  {
+    name: 'email',
+    label: 'Адрес электронной почты',
+    type: '',
+    options: {
+      required: 'Вы не заполнили это поле'
+    },
+    placeholder: 'Адрес электронной почты'
+  },
+  {
+    name: 'password',
+    label: 'Пароль',
+    type: '',
+    options: {
+      required: 'Вы не заполнили это поле'
+    },
+    placeholder: 'Пароль'
+  },
+  {
+    name: 'confirmPassword',
+    label: 'Подтверждение пароля',
+    type: '',
+    options: {
+      required: 'Вы не заполнили это поле'
+    },
+    placeholder: 'Подтверждение пароля'
+  }
+];
