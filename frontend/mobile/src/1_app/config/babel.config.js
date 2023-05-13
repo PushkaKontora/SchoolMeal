09aadcf28eef45769c19194e1c42aed449db7a63
SeriousDim <dlykov2001@yandex.ru>
2023-05-13 12:20:10 +0500

    build(frontend)!: babel.config.js changed

--- frontend/mobile/src/1_app/config/babel.config.js
+++ frontend/mobile/src/1_app/config/babel.config.js
@@ -2,16 +2,18 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
     'plugins': [
-      ['module:react-native-dotenv', {
+      [/*'module:react-native-dotenv', {
         'moduleName': '@env',
         'path': '.env',
         'blacklist': null,
         'whitelist': null,
         'safe': true,
         'allowUndefined': true
-      }]
+      },*/
+        'react-native-reanimated/plugin',
+        '@babel/plugin-proposal-export-namespace-from']
     ]
   };
 };
