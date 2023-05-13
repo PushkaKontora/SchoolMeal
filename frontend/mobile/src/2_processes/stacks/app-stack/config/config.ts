ec7a970a9980974da62094e3254f5fade786b0f2
SeriousDim <dlykov2001@yandex.ru>
2023-05-13 12:26:01 +0500

    feat(frontend): app-stack config updated: page name changed

--- frontend/mobile/src/2_processes/stacks/app-stack/config/config.ts
+++ frontend/mobile/src/2_processes/stacks/app-stack/config/config.ts
@@ -1,10 +1,10 @@
 import {ScreenConfig} from '../../../../7_shared/lib/stack-creator';
-import {MenuPage} from '../../../../3_pages/menu-page';
+import {FillerPage} from '../../../../3_pages/filler-page';
 import {StackNavigationOptions} from '@react-navigation/stack';
 
 export const SCREENS: ScreenConfig = {
   'Menu': {
-    component: MenuPage
+    component: FillerPage
   }
 };
 
