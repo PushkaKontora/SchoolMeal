12a485cb64002e3aafc3f6912d4a23fd6e40ff34
SeriousDim <dlykov2001@yandex.ru>
2023-05-13 12:35:49 +0500

    feat(frontend)!: authorization added for login & signup widgets

--- frontend/mobile/src/4_widgets/login-widget/ui/login-widget.tsx
+++ frontend/mobile/src/4_widgets/login-widget/ui/login-widget.tsx
@@ -2,13 +2,37 @@ import {MarginArea} from '../../../7_shared/ui/styling/margin-area';
 import {HorizontalLine} from '../../../7_shared/ui/styling/horizontal-line';
 import {AuthFeature} from '../../../5_features/auth-feature/ui/auth-feature';
 import {HEADER_TITLE, SUBHEADER_TITLE} from '../consts/consts';
-import {LoginForm} from '../../../6_entities/user/ui/login-form';
+import {LoginForm, LoginFormData} from '../../../6_entities/user';
 import {LoginPageProps} from '../../../3_pages/login-page/model/props';
 import {ButtonSecondary} from '../../../7_shared/ui/buttons/button-secondary/button-secondary';
+import {AUTH_API, TokenResponse} from '../../../5_features/auth';
+import {AuthTokenService} from '../../../5_features/auth';
+import {setAuthorized} from '../../../5_features/auth/model/auth-slice/auth-slice';
+import {useAppDispatch} from '../../../../store/hooks';
 
 export function LoginWidget({navigation}: LoginPageProps) {
   const navigateToSignUp = () => {
     navigation.navigate('SignUp');
+  };
+
+  const [signIn, {error}] = AUTH_API.useSignInMutation();
+  const dispatch = useAppDispatch();
+
+  const saveToken = async (currentData: LoginFormData) => {
+    const response: {data: TokenResponse} = await signIn(currentData);
+    await AuthTokenService.saveAuthToken(response.data);
+    dispatch(setAuthorized(true));
+
+    console.log(response.data);
+  };
+
+  const onSubmit = (currentData: LoginFormData) => {
+    saveToken(currentData);
+  };
+
+  const onError = (error: any) => {
+    console.log('Error:');
+    console.log(error);
   };
 
   return (
@@ -19,7 +43,9 @@ export function LoginWidget({navigation}: LoginPageProps) {
       <MarginArea
         marginBottom={16}>
 
-        <LoginForm/>
+        <LoginForm
+          onSubmit={onSubmit}
+          onError={onError}/>
 
       </MarginArea>
 
--- frontend/mobile/src/4_widgets/signup-widget/ui/sign-up-widget.tsx
+++ frontend/mobile/src/4_widgets/signup-widget/ui/sign-up-widget.tsx
@@ -4,11 +4,46 @@ import {HEADER_TITLE, SUBHEADER_TITLE} from '../consts/consts';
 import {MarginArea} from '../../../7_shared/ui/styling/margin-area';
 import {HorizontalLine} from '../../../7_shared/ui/styling/horizontal-line';
 import {ButtonSecondary} from '../../../7_shared/ui/buttons/button-secondary/button-secondary';
-import {SignUpForm} from '../../../6_entities/user/ui/sign-up-form';
+import {SignUpForm, USER_API} from '../../../6_entities/user';
+import {SignUpFormData} from '../../../6_entities/user/ui/sign-up-form/types';
+import {AUTH_API, AuthTokenService, TokenResponse} from '../../../5_features/auth';
+import {setAuthorized} from '../../../5_features/auth/model/auth-slice/auth-slice';
+import {useAppDispatch} from '../../../../store/hooks';
 
 export function SignUpWidget({navigation}: SignUpProps) {
   const navigateToLogin = () => {
     navigation.goBack();
+  };
+
+  const [signIn, signInInfo] = AUTH_API.useSignInMutation();
+  const [register, registerInfo] = USER_API.useRegisterMutation();
+
+  const dispatch = useAppDispatch();
+
+  const asyncRegister = async (currentData: SignUpFormData) => {
+    try {
+      const {confirmPassword, ...data} = currentData;
+      const loginData = {login: data.phone, password: data.password};
+
+      await register(data);
+
+      const response: {data: TokenResponse} = await signIn(loginData);
+      await AuthTokenService.saveAuthToken(response.data);
+      dispatch(setAuthorized(true));
+
+      console.log(response.data);
+    } catch (error) {
+      console.log('Cannot register: ');
+      console.log(error);
+    }
+  };
+
+  const onSubmit = (data: SignUpFormData) => {
+    asyncRegister(data);
+  };
+
+  const onError = (error: any) => {
+    console.log('Register error: ' + JSON.stringify(error));
   };
 
   return (
@@ -19,7 +54,9 @@ export function SignUpWidget({navigation}: SignUpProps) {
       <MarginArea
         marginBottom={16}>
 
-        <SignUpForm/>
+        <SignUpForm
+          onSubmit={onSubmit}
+          onError={onError}/>
 
       </MarginArea>
 
