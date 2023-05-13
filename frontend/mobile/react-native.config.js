300fada412fef4cec3d5efe5a507fd011c180275
SeriousDim <dlykov2001@yandex.ru>
2023-05-13 12:36:11 +0500

    feat(frontend)!: react-native.config.js added

---
+++ frontend/mobile/react-native.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  dependencies: {
+    'react-native-config': {
+      platforms: {
+        ios: null,
+        android: null
+      },
+    },
+  },
+};
