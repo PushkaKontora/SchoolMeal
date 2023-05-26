1df958ff2fd5a81af7ead423bc1585cc8e749a79
SeriousDim <dlykov2001@yandex.ru>
2023-05-26 11:09:48 +0500

    feat(frontend): child/api/config.ts: intents fixed

--- frontend/mobile/src/6_entities/child/api/config.ts
+++ frontend/mobile/src/6_entities/child/api/config.ts
@@ -1,44 +1,60 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
-import {BASE_BACKEND_URL} from "../../../7_shared/api/config";
-import {Child} from "../model/child";
-import {FindChildBody} from "./types";
-import {AuthTokenService} from "../../../5_features/auth";
-import {addAuthHeader} from "../../../7_shared/api";
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {BASE_BACKEND_URL} from '../../../7_shared/api/config';
+import {Child} from '../model/child';
+import {FindChildBody} from './types';
+import {AuthTokenService} from '../../../5_features/auth';
+import {addAuthHeader} from '../../../7_shared/api';
+import {ChildMealData, ChildMealDataWithId} from '../types/child-meal-data';
 
 export const CHILD_API = createApi({
-    reducerPath: 'api/children',
-    baseQuery: fetchBaseQuery({
-        baseUrl: BASE_BACKEND_URL + '/child',
-        prepareHeaders: async (headers, {getState}) => {
-            const token = await AuthTokenService.getToken();
-            if (token) {
-                return addAuthHeader(headers, token);
-            }
-        }
+  reducerPath: 'api/children',
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_BACKEND_URL + '/children',
+    prepareHeaders: async (headers, {getState}) => {
+      const token = await AuthTokenService.getToken();
+      if (token) {
+        return addAuthHeader(headers, token);
+      }
+    }
+  }),
+  tagTypes: ['UserChildren'],
+  endpoints: build => ({
+    findChildOnID: build.mutation<Child, FindChildBody>({
+      query: (body) => ({
+        url: '',
+        method: 'POST',
+        body: body,
+      }),
+      //  invalidatesTags: [{type: 'UserChildren', id: 'LIST'}]
     }),
-    tagTypes: ['UserChildren'],
-    endpoints: build => ({
-        findChildOnID: build.mutation<Child, FindChildBody>({
-            query: (body) => ({
-                url: '',
-                method: 'POST',
-                body: body,
-            }),
-            //  invalidatesTags: [{type: 'UserChildren', id: 'LIST'}]
-        }),
-        getUserChild: build.query<Child[], void>({
-            query: () => ({
-                url: ''
-            }),
-            // providesTags: (result) =>
-            //     result
-            //         ? [
-            //             ...result.map(({ id }) => ({ type: 'UserChildren' as const, id })),
-            //             { type: 'UserChildren', id: 'LIST' },
-            //         ]
-            //         : [{ type: 'UserChildren', id: 'LIST' }],
-        })
+    getUserChild: build.query<Child[], void>({
+      query: () => ({
+        url: ''
+      }),
+      // providesTags: (result) =>
+      //     result
+      //         ? [
+      //             ...result.map(({ id }) => ({ type: 'UserChildren' as const, id })),
+      //             { type: 'UserChildren', id: 'LIST' },
+      //         ]
+      //         : [{ type: 'UserChildren', id: 'LIST' }],
+    }),
+    changeMealPlan: build.mutation<ChildMealData, ChildMealDataWithId>({
+      query: ({childId, ...body}) => ({
+        url: `/${childId}`,
+        method: 'PATCH',
+        body: body
+      })
+    }),
+    getChildById: build.query<Child, string>({
+      query: (childId) => ({
+        url: `/${childId}`
+      })/*,
+      transformResponse(response: Child): Child {
+        return transformChild(response);
+      }*/
     })
+  })
 });
 
-export const {useGetUserChildQuery, useFindChildOnIDMutation} = CHILD_API;
+export const {useGetUserChildQuery, useFindChildOnIDMutation, useChangeMealPlanMutation, useGetChildByIdQuery} = CHILD_API;
