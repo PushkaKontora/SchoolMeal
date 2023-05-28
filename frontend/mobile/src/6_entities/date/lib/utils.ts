4f2e9d5c59451560cf770c9d986f37fabee4f809
SeriousDim <dlykov2001@yandex.ru>
2023-05-28 16:16:53 +0500

    feat(frontend): entities/date updated

--- frontend/mobile/src/6_entities/date/lib/utils.ts
+++ frontend/mobile/src/6_entities/date/lib/utils.ts
@@ -6,3 +6,11 @@ export function formatDate(d: Date) {
   const endPosition = formattedDate.length - 3;
   return formattedDate.slice(0, endPosition) + ' ' + STRINGS.ru.year;
 }
+
+export function dateToISOWithoutTime(d: Date): string {
+  return [
+    d.getFullYear(),
+    (d.getMonth()+1).toString().padStart(2, '0'),
+    d.getDate().toString().padStart(2, '0')
+  ].join('-');
+}
