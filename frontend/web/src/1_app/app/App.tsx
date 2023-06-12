fe53831c597dae21b419393915fd7f642b106724
SeriousDim <dlykov2001@yandex.ru>
2023-06-12 23:19:32 +0500

    feat(web.app): app.tsx updated

--- frontend/web/src/1_app/app/App.tsx
+++ frontend/web/src/1_app/app/App.tsx
@@ -1,11 +1,13 @@
 import '../styles/App.css'
+import {Provider} from 'react-redux';
+import {store} from '../../../store/store';
+import {AppNavigator} from '../../2_processes/app-navigator';
 
 function App() {
-
   return (
-    <>
-      <div>Web app</div>
-    </>
+    <Provider store={store}>
+      <AppNavigator/>
+    </Provider>
   )
 }
 
