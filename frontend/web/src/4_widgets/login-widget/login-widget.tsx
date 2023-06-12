3fc24dacec99435eb83dabd6df6e86747404b33c
SeriousDim <dlykov2001@yandex.ru>
2023-06-12 23:16:27 +0500

    feat(web.widgets): login-widget created

---
+++ frontend/web/src/4_widgets/login-widget/login-widget.tsx
@@ -0,0 +1,24 @@
+import {Content} from '../../7_shared/ui/markup/content';
+import {LoginForm} from '../../5_features/auth/ui/login-form';
+import {AuthTokenService, TokenResponse, useSignInMutation} from '../../5_features/auth';
+import {LoginFormData} from '../../5_features/auth/ui/login-form/types';
+import {setAuthorized} from '../../5_features/auth/model/auth-slice/auth-slice';
+import {useAppDispatch} from '../../../store/hooks';
+
+export function LoginWidget() {
+  const [signIn, signInInfo] = useSignInMutation();
+  const dispatch = useAppDispatch();
+
+  const onSubmit = async (data: LoginFormData) => {
+    const response: {data: TokenResponse} = await signIn(data);
+    await AuthTokenService.saveAuthToken(response.data);
+    dispatch(setAuthorized(true));
+  };
+
+  return (
+    <Content>
+      <LoginForm
+        onSubmit={onSubmit}/>
+    </Content>
+  );
+}
