a310a5bd93515dece51ba686edef8247025fd63e
SeriousDim <dlykov2001@yandex.ru>
2023-10-11 19:01:58 +0500

    feat(web.widgets): реализован meal-application-widget

---
+++ frontend/web/src/4_widgets/meal-application-widget/config/config.ts
@@ -0,0 +1,6 @@
+export const TITLE = 'Заявка на питание';
+
+export const CLASS_TITLES = {
+  elementary: '1-4 класс',
+  high: '5-11 класс'
+}
--- frontend/web/src/4_widgets/meal-application-widget/ui/meal-application-widget.tsx
+++ frontend/web/src/4_widgets/meal-application-widget/ui/meal-application-widget.tsx
@@ -1,24 +1,94 @@
-import {TableContainer} from './styles';
+import {TableContainer, Tabs, Title} from './styles';
 import {MealClassTable} from '../../../5_features/meals/meal-class-table';
 import {MealInfoTable} from '../../../5_features/meals/meal-info-table';
 import {Content} from '../../../7_shared/ui/markup/content';
+import {useGetMealRequestsQuery} from '../../../6_entities/meals/api/api';
+import {GetMealRequestsParams} from '../../../6_entities/meals/api/types';
+import {useEffect, useState} from 'react';
+import {MealRowData} from '../types/meal-row-data';
+import {sortMealsOfDate} from '../lib/utils';
+import {BREAKFAST_TITLE, DINNER_TITLE, LUNCH_TITLE} from '../consts/titles';
+import HeaderTeacherWidget from '../../header-teacher/ui/header-teacher';
+import {CLASS_TITLES, TITLE} from '../config/config';
+import {ButtonPrimary} from '../../../7_shared/ui/buttons/button-primary';
+import {ButtonSecondary} from '../../../7_shared/ui/buttons/button-secondary';
 
 export function MealApplicationWidget() {
+  const params: GetMealRequestsParams = {
+    date: '2023-06-14'
+  };
+  const {data: mealRequests} = useGetMealRequestsQuery(params);
+
+  const [mealRowData, setMealRowData] = useState<MealRowData | null>(null);
+  //const [totalMealRowData, setTotalMealRowData] = useState<MealRowValue | null>(null);
+
+  useEffect(() => {
+    if (mealRequests) {
+      setMealRowData(sortMealsOfDate(mealRequests));
+    }
+  }, [mealRequests]);
+
+  /*
+  useEffect(() => {
+    if (mealRowData) {
+      setTotalMealRowData(createTotalRowData(mealRowData));
+    }
+  }, [mealRowData]);
+   */
+
   return (
     <div>
       <Content>
+        <HeaderTeacherWidget/>
+
+        <Title>
+          {TITLE}
+        </Title>
+
+        <Tabs>
+          <ButtonSecondary
+            title={CLASS_TITLES.elementary}
+            onPress={() => {return;}}/>
+          <ButtonSecondary
+            title={CLASS_TITLES.high}
+            disabled
+            onPress={() => {return;}}/>
+        </Tabs>
+
         <TableContainer>
           <MealClassTable
-            width={'25%'}/>
+            width={'25%'}
+            classNames={(mealRowData && Object.keys(mealRowData)) || []}/>
 
           <MealInfoTable
-            width={'25%'}/>
+            width={'25%'}
+            title={BREAKFAST_TITLE}
+            showMissingValues={true}
+            data={
+              (mealRowData &&
+                Object.values(mealRowData)
+                  .map(item => item.breakfast)) || []
+            }/>
 
           <MealInfoTable
-            width={'25%'}/>
+            width={'25%'}
+            title={LUNCH_TITLE}
+            showMissingValues={true}
+            data={
+              (mealRowData &&
+                Object.values(mealRowData)
+                  .map(item => item.lunch)) || []
+            }/>
 
           <MealInfoTable
-            width={'25%'}/>
+            width={'25%'}
+            title={DINNER_TITLE}
+            showMissingValues={true}
+            data={
+              (mealRowData &&
+                Object.values(mealRowData)
+                  .map(item => item.dinner)) || []
+            }/>
         </TableContainer>
       </Content>
     </div>
