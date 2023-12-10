bc54d2a251187773ccbbff8dbe6795e7adb96510
tania <taniakovesh@gmail.com>
2023-12-10 11:14:14 +0500

    feat(web): таблица питания для учителя

--- frontend/web/src/7_shared/ui/checkbox/basic-checkbox/model/props.ts
+++ frontend/web/src/7_shared/ui/checkbox/basic-checkbox/model/props.ts
@@ -2,5 +2,9 @@ import { PropsWithChildren } from 'react';
 
 export type BasicCheckboxProps = {
   isDisable: boolean;
-  isCheck?: boolean;
+  isCheck: boolean;
+  onChange?: (state: boolean) => void;
+  type: string;
+  isHeader: boolean;
+  //'b'|| 'l'||'s'
 } & PropsWithChildren;
