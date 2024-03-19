b847e67ccb2f9763dabed5d3f5e59455e7e545d6
SeriousDim <dlykov2001@yandex.ru>
2024-03-19 14:36:27 +0500

    fix(web): добавлена 'dom' в lib в tsconfig.json

--- frontend/mobile/src/7_shared/api/config.ts
+++ frontend/mobile/src/7_shared/api/config.ts
@@ -1,6 +1,10 @@
-export const BASE_BACKEND_URL = 'http://10.0.2.2';
+//export const BACKEND_URL = 'http://188.120.237.106'
+export const BACKEND_URL = 'http://10.0.2.2';
+export const BASE_BACKEND_URL = `${BACKEND_URL}/api`;
 
 export const HEADERS = {
   Authorization: 'Authorization',
   xSignature: 'X-Signature'
 };
+
+export const SERVER_TIMEZONE: NonNullable<Intl.DateTimeFormatOptions['timeZone']> = 'Asia/Yekaterinburg';
